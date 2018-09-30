81848a01e1d48b79ae7cd5e31f5107883b17a369
apivovarchik <sf_beton81@mail.ru>
2018-09-30 11:25:31 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let i = 1;
+  let res = 0;
+  let j = 1;
+  while (j >= 1) {
+    j = number / Math.pow(5, i);
+    res += Math.floor(j);
+    i++;
+  }
+  return res;
 }
